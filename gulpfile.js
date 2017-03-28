44e196c65c17fa323207890eea8de623589756b2
Corjen Moll <corjen@me.com>
2017-03-28 14:24:36 +0200

    Autoload ip adress

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,16 @@
 /**
- * Gulpfile
- *
- * @since 1.0.0
- */
-var requireDir = require('require-dir')
+* Gulpfile
+*
+* @since 1.0.0
+*/
+const devIp = require('dev-ip')()
+const host = devIp[0] || 'localhost'
+const requireDir = require('require-dir')
+const fs = require('fs')
 requireDir('./tasks')
+
+fs.writeFile('src/ip.json', JSON.stringify({
+  ip: host
+}), err => {
+  if (err) throw err
+})
--- src/lib/Settings.php
+++ src/lib/Settings.php
@@ -10,12 +10,6 @@ namespace Example\Lib;
 
 class Settings {
 
-  /**
-   * The local url where the webpack bundle can be found
-   * @var string
-   */
-  var $webpackLocal = '//10.22.10.104:8080';
-
   public function __construct() {
     add_action( 'wp_enqueue_scripts', array( $this, 'enqueueAssets' ) );
   }
@@ -24,10 +18,13 @@ class Settings {
    * Enqueue CSS & JS assets
    */
   public function enqueueAssets() {
+    wp_enqueue_script( 'polyfill', 'https://cdn.polyfill.io/v2/polyfill.min.js', '', array(), true );
+
     /* If we're in the development enviroment, load the unhashed css and webpack bundle */
     if ( WP_ENV === 'dev' ) {
-      wp_enqueue_script( 'main', $this->webpackLocal . '/bundle.js', array(), '', true );
-      wp_enqueue_style( 'main', get_stylesheet_directory_uri() . '/css/main.css', array(), '', 'screen' );
+      $ip = json_decode( file_get_contents( get_stylesheet_directory() . '/ip.json' ) );
+      wp_enqueue_style( 'main', get_stylesheet_directory_uri() . '/css/main.css' );
+      wp_enqueue_script( 'main', '//' . $ip->ip . ':8080/bundle.js', '', array(), true );
     } else {
       /* Read css assets and load file */
       $assets = (array) json_decode( file_get_contents( get_stylesheet_directory() . '/css/css-assets.json' ) );
