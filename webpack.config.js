ad765dc6f77f64c117d649571bd183fa80e29726
Corjen Moll <corjen@me.com>
2017-04-19 10:16:58 +0200

    Fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,26 @@
 const webpack = require('webpack')
-const path = require('path')
 const devIp = require('dev-ip')()
 const config = require('./gulpconfig')
 const host = devIp[0] || 'localhost'
 
+console.log(config.webpackPublicPath)
+
 module.exports = {
-  entry: './src/js/main.js',
-  // entry: [
-  //   'webpack/hot/dev-server',
-  //   'webpack-dev-server/client?http://' + host + ':8080/',
-  //   'src/js/main.js'
-  // ],
+  entry: [
+    './src/js/main.js'
+  ],
   output: {
-    path: path.resolve(__dirname, config.webpackPublicPath),
     filename: 'bundle.js',
-    publicPath: path.resolve(__dirname, config.webpackPublicPath)
+    publicPath: '/'
   },
   devtool: 'source-map',
   module: {
     rules: [
       {
         test: /\.js?$/,
-        use: ['babel-loader'],
+        use: [
+          'babel-loader'
+        ],
         exclude: /node_modules/
       }
     ]
@@ -32,12 +31,7 @@ module.exports = {
   ],
   devServer: {
     port: 8080,
-    contentBase: path.resolve(__dirname, config.webpackPublicPath),
-    historyApiFallback: true,
     hot: true,
-    host: host,
-    proxy: {
-      '*': 'http://localhost:3000'
-    }
+    host: host
   }
 }
